faec172d4ae81807290f386d10c8d9d606bb56c0
LucianWeber <git@lucian.email>
2015-03-26 17:49:50 +0100

    Release
    Complete Rewrite

--- main.js
+++ main.js
@@ -1,37 +1,41 @@
-/// <reference path="https://code.jquery.com/jquery-1.10.2.js"/>
+(function (module, window, undefined) {
 
-var Lucian = new lucian();
+	function print(obj) {
+		var content = "";
+		for(var i = 0; i < obj.blocks.length; i++) {
+			var block = obj.blocks[i];
+			content += "<h4>" + block.headline + "</h4><p>";
+			for(var j = 0; j < block.lines.length; j++) {
+				var line = block.lines[j];
+				content += line.key + ": ";
+				if(typeof line.value === "object" && line.value.length !== undefined) {
+					line.value = line.value.join(", ");
+				}
+				content += line.value + "<br>";
+			}
+			content += "</p>";
+		}
+		window.document.getElementsByClassName('text')[0].innerHTML = content;
 
-function lucian() {
-    //Private
-    var that = this;
-    var url = window.location.href;
+		window.document.getElementById('name').innerHTML = obj.me.name;
+		window.document.getElementById('title').innerHTML = obj.me.title;
+		window.document.getElementById('mail').innerHTML = obj.me.mail.replace("@", "[at]");
+		window.document.getElementById('mail').href = "mailto:" + obj.me.mail;
+		window.document.getElementById('image').src = obj.me.image;
+	}
 
-    //Public
-    this.ContentID = null;
+	function init() {
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+		    if (request.readyState == 4 && request.status == 200) {
+		        var json = JSON.parse(request.responseText);
+		        print(json);
+		    }
+		}
+		request.open("GET", "content.json", true);
+		request.send();
+	}
 
-    this.Init = function () {
-        var array = url.split("#");
-        if (array.length > 1) {
-            that.ContentID = array[1].toLowerCase();
-        } else {
-            that.ContentID = "main";
-        }
-        that.LoadContent();
-    }
+	init();
 
-    this.LoadContent = function (name) {
-        if (name == null)
-            name = that.ContentID;
-
-        $("#templateWrapper").append($("#temp_" + that.ContentID));
-        $("#contentBox").html($("#temp_" + name));
-        that.ContentID = name;
-
-        if ($("#contentBox").html().trim() == "") {
-            $("#contentBox").html($("#temp_error"));
-            that.ContentID = "error";
-        }
-    }
-
-}
+})((window.l = {}), window);
