47a0a17080858d12416931119f51d2a4cef4361a
Lucian Weber <git@lucian.email>
2019-10-07 13:45:42 +0200

    remove google analytics

--- content/script.js
+++ content/script.js
@@ -1,15 +1,3 @@
-// Google Analytics
-(function() {
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-61547324-1', 'auto');
-    ga('send', 'pageview');
-})();
-
-
 (function(document) {
 
     document
@@ -29,6 +17,6 @@
                 .toggle('small');
         });
     
-    console.log("%cDear visitor,\n\n%cyou can find all sourcefiles of this website in its public github repository. [https://github.com/lucianw/weber.xyz]\nIf you want to contact me, choose one of the listed platforms and drop me a message.\n\nHave a great day,\nLucian", "font-size: 20px","font-size: 16px");
+    console.log("%cDear visitor,\n\n%cyou can find all sourcefiles of this website in its public github repository. [https://github.com/lucianweber/weber.xyz]\nIf you want to contact me, choose one of the listed platforms and drop me a message.\n\nHave a great day,\nLucian", "font-size: 20px","font-size: 16px");
 
 })(window.document);
